df32113fb101b8378030195a447e546f906cbdc8
GalZi <59266283+Galzi1@users.noreply.github.com>
2021-03-20 12:41:21 +0200

    Added EnumViewer for displaying enum values (getting options from enum type definition).

--- src/Components/TypeViewers/EnumViewer.js
+++ src/Components/TypeViewers/EnumViewer.js
@@ -1,22 +1,67 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {isNumber, isFloat} from '../../Utils/TypesUtils';
+import {isStringNumeric} from '../../Utils/GeneralUtils';
+import PrimitiveViewer from './PrimitiveViewer';
+import shortid from 'shortid';
 
 export default function EnumViewer(props) {
     const field = props.field;
-    // "name": "Prim1", 
-    // "type": "Int32", 
+    // "name": "Enum1", 
+    // "type": "E_Mode", 
     // "isArray": false, 
     // "units": "", 
-    // "range": "0-1000", 
-    // "scale": "1", 
-    // "description": "A Primitive Example", 
-    // "value": 123
+    // "range": "", 
+    // "scale": "", 
+    // "description": "An Enum Example", 
+    // "value": 1
+    const enumType = props.enumType;
+    // "name": "E_Mode", 
+    // "basetype": "Byte", 
+    // "values": [
+    //     {
+    //         "name": "DISCONNECTED", 
+    //         "value": 0
+    //     }, 
+    //     {
+    //         "name": "CONNECTED", 
+    //         "value": 1
+    //     }, 
+    //     {
+    //         "name": "PENDING", 
+    //         "value": 2
+    //     }
+    // ]
     const onFieldValueUpdated = props.onFieldValueUpdated;
 
-    const [value, setValue] = useState(field.value);
+    const [currValue, setCurrValue] = useState(field.value);
+    const [enumOptions, setEnumOptions] = useState([]);
+
+    useEffect(() => {
+        if (!(Object.is(enumType, undefined) || Object.is(enumType, null))) {
+            getEnumOptions(enumType);
+        }
+    }, [enumType]);
+
+    const getEnumOptions = (enumType) => {
+        setEnumOptions(enumType.values);
+    };
+
+    const onEnumValueChanged = (enumValue) => {
+        setCurrValue(enumValue.value);
+        onFieldValueUpdated();
+    };
 
     return (
         <div id="enum-viewer-div">
-            {field.type}
+            <select value={currValue} 
+            onChange={onEnumValueChanged} 
+            className="form-control form-control-sm">
+            {
+                enumOptions.map((enumOption, index) =>
+                    <option key={shortid.generate()} value={enumOption.value}>{enumOption.name}</option>
+                )
+            }
+            </select>
         </div>
-    )
+    );
 }
